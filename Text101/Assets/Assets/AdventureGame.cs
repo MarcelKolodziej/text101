971deeefc2483aa9c38a646b23a783f886d66aae
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-05-29 12:02:46 +0200

    State machines, lessson 25

---
+++ Text101/Assets/Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+
+    State state;
+
+    void Start()
+    {
+       state = StartingState;
+       textComponent.text = state.GetStateStory();
+    }
+
+    
+}
