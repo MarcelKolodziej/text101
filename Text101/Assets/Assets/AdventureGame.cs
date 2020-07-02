97e23ac66154dfd130ecfdcbc3bfc4799d1169d8
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-07-02 12:25:28 +0200

    26 lesson

--- Text101/Assets/Assets/AdventureGame.cs
+++ Text101/Assets/Assets/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,5 +17,29 @@ public class AdventureGame : MonoBehaviour
        textComponent.text = state.GetStateStory();
     }
 
-    
+    void Update() 
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            Debug.Log("Pressed 1");
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            Debug.Log("Pressed 2");
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+            Debug.Log("Pressed 3");
+        }
+        textComponent.text = state.GetStateStory();
+    }
 }
